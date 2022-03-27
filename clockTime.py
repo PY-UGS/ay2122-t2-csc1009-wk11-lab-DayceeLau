9bf3e497551b3edeccdaa203fada8a0a1a44b4c8
DayceeLau <97490603+DayceeLau@users.noreply.github.com>
2022-03-27 23:34:14 +0800

    Update clockTime.py

--- clockTime.py
+++ clockTime.py
@@ -1,6 +1,6 @@
 class clockTime:
     def __init__(self):
-        self.hrs = 0
+        self.hrs = 0    #Instantiate values to 0
         self.mins = 0
         self.sec = 0
     
@@ -14,9 +14,9 @@ class clockTime:
         self.sec = sec
     
     def setTime(self, hrs, mins, sec):
-        self.setHours(hrs)
-        self.setMinutes(mins)
-        self.setSec(sec)
+        self.setHours(hrs)          #Calls setHours method in clockTime take in hrs
+        self.setMinutes(mins)       #Calls setMinutes method in clockTime take in mins
+        self.setSec(sec)            #Calls setSeconds method in clockTime take in sec
     
     def showTime(self):
         time = str(self.hrs + ":" + self.mins + ":" + self.sec)
